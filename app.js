cddf779c2152467c9ed344c74f5bf3f793be1416
Büşra <busraayazicioglu@outlook.com>
2025-11-04 11:03:53 +0300

    feat: fonksiyon arayüze eklendi

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ function runEvents() {
 }
 
 function addTodo(e) {
+  const inputText = addInput.value.trim();
+  if (inputText == null || inputText == "") {
+    alert("Lütfen bir değer giriniz.");
+  } else {
+    addTodoToUI(inputText);
+  }
   console.log("submit eventi calisti.");
   e.preventDefault();
+}
+
+function addTodoToUI(newTodo) {
+  const li = document.createElement("li");
+  li.className = "list-group-item d-flex justify-content-between";
+  li.textContent = newTodo;
+  const a = document.createElement("a");
+  a.className = "delete-item";
+  a.href = "#";
+  const i = document.createElement("i");
+  i.className = " fa fa-remove";
+  a.appendChild(i);
+  li.appendChild(a);
+  todoList.appendChild(li);
 }
